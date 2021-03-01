61348eaf6bea6cc7885b6ca00778574f92556fc1
isaiah165 <Isaiahfelton17@gmail.com>
2021-02-28 20:49:50 -0600

    Implemented ORDER BY function to output all creationDates for contacts

--- ContactBook.py
+++ ContactBook.py
@@ -49,56 +49,68 @@ select_contacts = "SELECT * FROM contacts"
 contacts = execute_read_query(connection, select_contacts)
 
 
-
-# Menu interface 
-print(" Menu \n","a - Add contact \n","d - Remove contact \n","u - Update contact details \n","b - Output all contacts in alphebetical order\n","c - Output all contacts by creation date \n", "o - output all contacts \n","q - Quit ")
-value = input("choose and option: \n")
+# Menu interface
+while True:
+    print(" Menu \n","a - Add contact \n","d - Remove contact \n","u - Update contact details \n","b - Output all contacts in alphebetical order\n","c - Output all contacts by creation date \n", "o - output all contacts \n","q - Quit ")
+    value = input("choose and option: ")
 
 # Asking user for contact details and giving the user the creation dat of the newly added contact
-if value == "a":
-    contactDetails = input("Enter First Name: \n")
-    creationDate = date.today()
+    if value == "a":
+        contactDetails = input("Enter First Name: \n")
+        creationDate = date.today()
+        print("You created",contactDetails,"on",creationDate)
+        addContact = "INSERT INTO contacts (contactDetails, creationDate) VALUES ('%s','%s')" % (contactDetails, creationDate)
+        execute_query(connection, addContact)
+        continue
     
-    print("You created",contactDetails,"on",creationDate)
-    addContact = "INSERT INTO contacts (contactDetails, creationDate) VALUES ('%s','%s')" % (contactDetails, creationDate)
-    execute_query(connection, addContact)
 
-
-
+# Removing a user in the contacts table in MySql
+    if value == "d":   
+        for contact in contacts:
+            print(contact)
+        deleteDetails = input("Enter id of the person thats to be deleted: ")
+        deleteUser = "DELETE FROM contacts WHERE id = '%s'" % (deleteDetails)
+        execute_query(connection, deleteUser)
+        continue
 
 # Updating a user in the contacts table in MySql
-if value == "u":
-    for contact in contacts:
-        print(contact)
-    updateDetails = input("Enter updated first name: ")
-    updatedDetails = input("Enter id for the update: ")
-    updateDetails_query = """   
-    UPDATE contacts
-    SET contactDetails = '%s'
-    WHERE id = %s """ % (updateDetails, updatedDetails)   
-    execute_query(connection, updateDetails_query)                            
-    
-     
+    if value == "u":
+        for contact in contacts:
+            print(contact)
+        updateDetails = input("Enter updated first name: ")
+        updatedDetails = input("Enter id for the update: ")
+        updateDetails_query = """   
+        UPDATE contacts
+        SET contactDetails = '%s'
+        WHERE id = %s """ % (updateDetails, updatedDetails)   
+        execute_query(connection, updateDetails_query)                            
+        continue
 
-
-# Deleting a user in the contacts table in MySql
-if value == "d":   
-    for contact in contacts:
-        print(contact)
-    deleteDetails = input("Enter id of the person thats to be deleted: ")
-    deleteUser = "DELETE FROM contacts WHERE id = '%s'" % (deleteDetails)
-    execute_query(connection, deleteUser)
-
-
-# Printing all contacts in the contacts table to the console
-if value == "o":
-    for contact in contacts:
-        print(contact)
 
 # Outputting all contacts in alphbetical order
-if value =="b":
-    select_contactDetails = "SELECT * FROM contacts ORDER BY contactDetails" #used ORDER BY to sort the names in my contactDetails column
-    contactDetails = execute_read_query(connection, select_contactDetails)
-    for DetailsOfContact in contactDetails:
-        print(DetailsOfContact[1])
+    if value =="b":
+        select_contactDetails = "SELECT * FROM contacts ORDER BY contactDetails" #used ORDER BY to sort the names in my contactDetails column
+        contactDetails = execute_read_query(connection, select_contactDetails)
+        for DetailsOfContact in contactDetails:
+            print(DetailsOfContact[1])
+        continue
+
+# Outputting all contacts by creation date
+    if value =="c":
+        select_creationDate = "SELECT * FROM contacts ORDER BY creationDate" #used ORDER BY to sort the names in my creationDate column
+        creationDate = execute_read_query(connection, select_creationDate)
+        for DateOfCreation in creationDate:
+            print(DateOfCreation[2])
+        continue
+
+# Printing all contacts in the contacts table to the console
+    if value == "o":
+        for contact in contacts:
+            print(contact)
+        continue
+
+    if value == "q":
+        break
+    
+
 
